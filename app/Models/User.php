4e941060cf695f8d92d0dcd84c16253a2284f8d6
nzero3 <939511779@qq.com>
2018-03-06 08:48:04 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,13 @@ class User extends Authenticatable
      *
      * @var array
      */
+	
+	public function gravatar($size = '100')
+	{
+		$hash = md5(strtolower(trim($this->attributes['email'])));
+		return "http://www.gravatar.com/avatar/$hash?s=$size";
+	}
+	 
 	protected $table = 'users'; 
 	 
     protected $fillable = [
